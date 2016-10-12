e391d1f893dd333784ed0624d84190ea97fdf971
Andreas Knees <andreas.knees@nikem.de>
2016-10-12 12:00:38 +0200

    add WS client

--- src/main/webapp/scripts/nest/nestbase.js
+++ src/main/webapp/scripts/nest/nestbase.js
@@ -8,6 +8,62 @@ define("nest", ["jquery"], function($) {
 		 */
 		fouc : function () {
 			$("body").css("display", "");
-		}		
+		},
+
+		Ws : function (options) {
+			var ws, closeHandler;
+
+			closeHandler = $.proxy(function (event) {
+				if (options.onconnectionbroken) {
+					options.onconnectionbroken();
+				}
+				try {
+					ws.close();
+					ws.onerror = null;
+					ws.onclose = null;
+					ws.onopen = null;
+					ws.onmessage = null;
+					ws = null;
+				} catch (error) { }
+				try {
+					clearTimeout(t);				
+				} catch (error) { }
+				t = setTimeout($.proxy(this.init, this), 3000);
+			}, this);
+
+			this.init = function () {
+				ws = new WebSocket(options.uri);
+
+				ws.onerror = closeHandler;
+				ws.onclose = closeHandler;
+				ws.onopen = $.proxy(function (evt) {
+					console.log(evt);
+				}, this);
+
+				ws.onmessage = $.proxy(options.onmessage, this);
+
+			};
+
+			this.send = function (message) {
+				if (ws) {
+					ws.send(message);
+					console.log("sent message");
+				} else {
+					console.log("no connection");
+				}
+			}
+
+			this.ping = function () {
+				try {
+					ws.send("ping");
+					if (ws.readyState !== 1) {
+						closeHandler();
+					}
+				} catch (error) {
+					closeHandler();
+				}
+			};
+			setInterval(this.ping, 5000);
+		}
 	}
 });
